40660ae96327ca216385ef951a96a75e7e055742
hackdave21 <davalawi77@gmail.com>
2025-09-10 22:25:46 +0000

    creation de onboarding

---
+++ lib/mainhome_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MainhomeScreen extends StatefulWidget {
+  const MainhomeScreen({super.key});
+
+  @override
+  State<MainhomeScreen> createState() => _MainhomeScreenState();
+}
+
+class _MainhomeScreenState extends State<MainhomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/onboarding_screen.dart
+++ lib/onboarding_screen.dart
@@ -1,10 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:rvsapp/mainhome_screen.dart';
 
 class OnboardingScreen extends StatelessWidget {
   const OnboardingScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return  Scaffold(
+     body: Center(
+       child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text("Bienvenue", style: TextStyle(
+            color: const Color.fromARGB(255, 2, 70, 125),
+            fontSize: 17
+          ),),
+          // SizedBox(height: 150,),
+          Container(
+            margin: EdgeInsets.only(top: 390),
+            padding: EdgeInsets.all(7),
+            decoration: BoxDecoration(
+              color: const Color.fromARGB(255, 2, 70, 125), 
+              borderRadius: BorderRadius.circular(12),
+              border: Border.all(color: Colors.lightBlue)
+            ),
+            child: InkWell(
+              onTap: () {
+                Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => MainhomeScreen()),
+    );
+              },
+              child: Text("Passer", style: TextStyle(color: Colors.white, fontWeight: FontWeight.w500),),
+            ),
+          )
+        ],
+       ),
+     ),
+    );
   }
 }
