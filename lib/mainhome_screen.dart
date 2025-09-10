3c1d605beb3a7025398188657bb6221903e67ef0
hackdave21 <davalawi77@gmail.com>
2025-09-10 22:58:39 +0000

    bottomnavbar et les pages du menu

--- lib/mainhome_screen.dart
+++ lib/mainhome_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:rvsapp/pages/calendar_screen.dart';
+import 'package:rvsapp/pages/home_screen.dart';
+import 'package:rvsapp/pages/profile_screen.dart';
+import 'package:rvsapp/pages/settings_screen.dart';
 
 class MainhomeScreen extends StatefulWidget {
   const MainhomeScreen({super.key});
@@ -8,8 +12,53 @@ class MainhomeScreen extends StatefulWidget {
 }
 
 class _MainhomeScreenState extends State<MainhomeScreen> {
+  int _selectedIndex = 0;
+
+  final List<Widget> widgetOptions = [
+    HomeScreen(),
+    CalendarScreen(),
+    ProfileScreen(),
+    SettingsScreen()
+  ];
+
+  void _onItemTapped (int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(),
+       body: widgetOptions.elementAt(_selectedIndex),
+      bottomNavigationBar: BottomNavigationBar(
+        showSelectedLabels: true,
+        showUnselectedLabels: true,
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: const Color.fromARGB(255, 2, 70, 125),
+        unselectedItemColor: Colors.blueGrey,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+           BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_month),
+            label: 'Calendrier',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profil',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Paramètres',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+      ),
+    );
   }
 }
